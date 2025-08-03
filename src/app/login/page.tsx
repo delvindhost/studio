f0188acc90c9cacf3a25ead1163426a4c1a8fe8e
Douglas Vini <douglasdannuz@gmail.com>
2025-08-03 16:59:57 +0000

    o login t asem css

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-background">
       <Card className="w-full max-w-sm">
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
