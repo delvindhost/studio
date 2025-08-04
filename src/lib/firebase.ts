cd02a3a86b325436e795e94944496d573e46a550
Douglas Vini <douglasdannuz@gmail.com>
2025-08-04 21:05:04 +0000

    é esse app mesmop ? Nome do projeto controle-q ID do projeto controle-q

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,16 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAhDyVaRlMyxIwSAIMcsB15zLKKJYdTvt0",
   authDomain: "controle-q.firebaseapp.com",
   projectId: "controle-q",
-  storageBucket: "controle-q.appspot.com",
+  storageBucket: "controle-q.firebasestorage.app",
   messagingSenderId: "611076568023",
   appId: "1:611076568023:web:1673b0fc82d1f42584d192",
   measurementId: "G-52K5PJ8WE3"
@@ -21,5 +19,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+// Initialize Analytics if running in the browser
+if (typeof window !== 'undefined') {
+  getAnalytics(app);
+}
+
 
 export { app, auth, db };
