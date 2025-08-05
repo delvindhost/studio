4411ddf23d3c7834466b73c733d459c294e5453d
Douglas Vini <douglasdannuz@gmail.com>
2025-08-05 23:15:46 +0000

    ta so carregando aqui

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAhDyVaRlMyxIwSAIMcsB15zLKKJYdTvt0",
   authDomain: "controle-q.firebaseapp.com",
   projectId: "controle-q",
-  storageBucket: "controle-q.firebasestorage.app",
+  storageBucket: "controle-q.appspot.com",
   messagingSenderId: "611076568023",
   appId: "1:611076568023:web:1673b0fc82d1f42584d192",
   measurementId: "G-52K5PJ8WE3"
